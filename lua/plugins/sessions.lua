c1612afa45a5b6ab5a04aa921433a052b6586bc1
abdllahmaher <abdoullahnasr9@gmail.com>
2026-02-03 05:38:16 +0200

    Update nvim config

--- lua/plugins/sessions.lua
+++ lua/plugins/sessions.lua
@@ -1,1 +1,16 @@
-return { "rmagatti/auto-session" }
+-- return { "rmagatti/auto-session" }
+--
+--
+--
+return {
+  "rmagatti/auto-session",
+  lazy = false,
+
+  ---enables autocomplete for opts
+  ---@module "auto-session"
+  ---@type AutoSession.Config
+  opts = {
+    suppressed_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
+    -- log_level = 'debug',
+  },
+}
